ab90b2c326b5b53fe629e42e894779d8b8cf1693
Avelexis <alsydoruk642@gmail.com>
2026-05-20 15:32:49 +0200

    task-two

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,43 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const refs = {
+    form: document.querySelector('.form'),
+  };
+
+  if (!refs.form) return;
+
+  refs.form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const delay = Number(refs.form.elements.delay.value);
+    const state = refs.form.elements.state.value;
+
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (state === 'fulfilled') {
+          resolve(delay);
+        } else {
+          reject(delay);
+        }
+      }, delay);
+    });
+
+    promise
+      .then(delay => {
+        iziToast.success({
+          message: `✅ Fulfilled promise in ${delay}ms`,
+          position: 'topRight',
+        });
+      })
+      .catch(delay => {
+        iziToast.error({
+          message: `❌ Rejected promise in ${delay}ms`,
+          position: 'topRight',
+        });
+      });
+
+    refs.form.reset();
+  });
+});
